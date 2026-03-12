6bd3db6719b26f074fe2a597a8204ff31aab6c2c
Brooks Kahsai <brookskahsai@outlook.com>
2026-03-12 12:57:03 -0700

    Cleaned up implementation

--- Final_Project.py
+++ Final_Project.py
@@ -1,19 +1,10 @@
-import numpy as np
 import zipfile
-import matplotlib.pyplot as plt
-import pandas as pd
-import statsmodels as sm
+from logistic import *
+from preprocess import *
 
-# Preprocessing for dataset
-zip_path = 'weather_forecasts.csv.zip'
-with zipfile.ZipFile(zip_path, "r") as z:
-    files = [f for f in z.namelist() if not f.startswith("__MACOSX")]
+# Collecting Dataset from ZipFile
+with zipfile.ZipFile("weather_forecasts.csv.zip") as z:
+    weather_data = pd.read_csv(z.open("weather_forecasts.csv"))
 
-    with zipfile.ZipFile("clean_weather.zip", "w") as new_zip:
-        for f in files:
-            new_zip.writestr(f, z.read(f))
-
-# Collected dataset / unzipped csv
-weather_data = pd.read_csv('clean_weather.zip', compression='zip')
-# Drop N/A values
+# Drop N/A values across all rows
 weather_data.dropna(inplace=True)
