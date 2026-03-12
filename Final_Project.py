9ff7df5f55b6a31ed28d49133342a90bdec2fcfe
Brooks Kahsai <brookskahsai@outlook.com>
2026-03-12 12:49:15 -0700

    Added some comments

--- Final_Project.py
+++ Final_Project.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels as sm
 
+# Preprocessing for dataset
 zip_path = 'weather_forecasts.csv.zip'
 with zipfile.ZipFile(zip_path, "r") as z:
     files = [f for f in z.namelist() if not f.startswith("__MACOSX")]
@@ -12,5 +13,7 @@ with zipfile.ZipFile(zip_path, "r") as z:
         for f in files:
             new_zip.writestr(f, z.read(f))
 
+# Collected dataset / unzipped csv
 weather_data = pd.read_csv('clean_weather.zip', compression='zip')
+# Drop N/A values
 weather_data.dropna(inplace=True)
