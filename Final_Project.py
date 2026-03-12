9c03bfafe192b61429a9724d9f0f6ba79deea097
Brooks Kahsai <brookskahsai@outlook.com>
2026-03-12 12:45:01 -0700

    Fixed data access so we can run it properly

--- Final_Project.py
+++ Final_Project.py
@@ -1,9 +1,16 @@
 import numpy as np
+import zipfile
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels as sm
 
+zip_path = 'weather_forecasts.csv.zip'
+with zipfile.ZipFile(zip_path, "r") as z:
+    files = [f for f in z.namelist() if not f.startswith("__MACOSX")]
 
-weather_data = pd.read_csv("weather_forecasts.csv")
+    with zipfile.ZipFile("clean_weather.zip", "w") as new_zip:
+        for f in files:
+            new_zip.writestr(f, z.read(f))
+
+weather_data = pd.read_csv('clean_weather.zip', compression='zip')
 weather_data.dropna(inplace=True)
-print(weather_data.head())
